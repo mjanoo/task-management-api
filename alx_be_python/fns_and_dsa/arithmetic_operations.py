3b052d7b0e58e69290c309b18a95898235f95002
janet wambua <janet2wambua@gmail.com>
2025-07-26 17:47:52 +0300

    Fix perform_operation to return None on errors

--- alx_be_python/fns_and_dsa/arithmetic_operations.py
+++ alx_be_python/fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,13 @@
 def perform_operation(num1, num2, operation):
-    if operation == 'add':
+    if operation == "add":
         return num1 + num2
-    elif operation == 'subtract':
+    elif operation == "subtract":
         return num1 - num2
-    elif operation == 'multiply':
+    elif operation == "multiply":
         return num1 * num2
-    elif operation == 'divide':
+    elif operation == "divide":
         if num2 == 0:
-            return "Error: Division by zero"
+            return None  # Return None instead of a string error
         return num1 / num2
     else:
-        return "Error: Invalid operation"
+        return None  # Also return None for invalid operation
